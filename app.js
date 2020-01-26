461698619bb804bbabd2945631f181c1c4a26ba9
akashcoc <akashshetty022.as@gmail.com>
2020-01-26 15:20:33 +0530

    final code

--- app.js
+++ app.js
@@ -7,28 +7,72 @@ new Vue({
     playerHealth: 100,
     monsterHealth: 100,
     // so initially game should not be runing, thats why its set to false
-    gameIsRunning: false
+    gameIsRunning: false,
+    // storing logs 
+    turns : []
   },
   methods: {
     startGame: function() {
-      this.gameIsRunning = true;
-      this.playerHealth = 100;
-      this.monsterHealth = 100;
+        this.gameIsRunning = true;
+        this.playerHealth = 100;
+        this.monsterHealth = 100;
+        this.turns= [];
     },
     attack: function() {
-        this.monsterHealth  -= this.calculateDamage(3, 10);
-
-        if (this.checkWin()){
+        var damage = this.calculateDamage(3, 10);
+        this.monsterHealth  -= damage;
+        this.turns.unshift({
+            isPlayer: true,
+            text: "Player hits Monster for" + damage
+        });
+        if (this.checkWin()) {
             return;
         }
 
-        this.playerHealth -= this.calculateDamage(5, 12);
-        thi.checkWin();
+       this.monsterAttacks();
 
     },
-    specialAttck: function() {},
-    heal: function() {},
-    giveUp: function() {},
+    specialAttack: function() {
+        var damage = this.calculateDamage(10, 20);
+        this.monsterHealth  -= damage;
+        this.turns.unshift({
+            isPlayer: true,
+            text: "Player hits Monster Special Attack for" + damage
+        })
+        if (this.checkWin()) {
+            return;
+        }
+       
+        this.monsterAttacks();
+
+    },
+    heal: function() {
+        if(this.playerHealth <= 90){
+            this.playerHealth += 10;
+           
+        } else {
+            this.playerHealth = 100;
+        }
+        this.turns.unshift({
+            isPlayer: true,
+            text: "Player heals for 10"
+        })
+        this.monsterAttacks();
+        
+    },
+    giveUp: function() {
+
+        this.gameIsRunning = false;
+    },
+    monsterAttacks : function() {
+        var damage = this.calculateDamage(5, 12);
+        this.playerHealth -= damage;
+        this.checkWin();
+        this.turns.unshift({
+            isPlayer: false,
+            text: "Monster hits player for" + damage
+        });
+    },
     // "Math.random()"  damage value is from 0 and 1
     // "Math.random() * max" damage  value is from 0 and 9.999999
     // "Math.floor(Math.random() * max)"damage value is from 0 and 9
@@ -52,8 +96,9 @@ new Vue({
           } else {
             this.gameIsRunning = false;
           }
-          return true;
+          return true;          
       }
+      return false;
     }
   }
 });
